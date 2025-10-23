100ddc0b0a8063c250f21e8944ff2ef4d0079598
Misha Ip-511 <konovalenkomihail62gmail.com>
2025-10-24 02:46:34 +0700

    1

--- src/advanced_array_function.c
+++ src/advanced_array_function.c
@@ -1,7 +1,24 @@
 #include "../include/advanced_array_function.h"
 
 int max_subarray_sum(int* nums, int size) {
-    return 0;
+
+    int maxs = nums[0];   
+    int current_sum = nums[0]; 
+
+    for (int i = 1; i < size; i++) {
+        
+        if (current_sum < 0) {
+            current_sum = nums[i];
+        } else {
+            current_sum += nums[i];
+        }
+
+        if (current_sum > maxs) {
+            maxs = current_sum;
+        }
+    }
+
+    return maxs;
 }
 
 int length_of_lis(int* nums, int numsSize) {
