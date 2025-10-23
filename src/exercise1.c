bb0aa1e38cc41d976717e28bc3b3ecb00835f01b
Misha Ip-511 <konovalenkomihail62gmail.com>
2025-10-23 23:46:49 +0700

    N1 done

--- src/exercise1.c
+++ src/exercise1.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main(int argc, char** argv) {
-    printf("Hello World!");
+int main(int argc,char** argv) {
+    double num[10];
+    double sum = 0;
+    scanf("%lf %lf %lf %lf %lf %lf %lf %lf %lf %lf", &num[0], &num[1], &num[2], &num[3], &num[4], &num[5], &num[6], &num[7], &num[8], &num[9]);
+    for (int i = 0; i < 10; i++) {
+        sum += num[i];
+    }
+    printf("%.3lf ", (sum / (double)10));
     return 0;
 }
